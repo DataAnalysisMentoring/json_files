feaa8801b671dcbd08df58075bce1b8b415b5607
hahahanni <hahahanni@gmail.com>
2018-09-05 21:33:04 +0200

    cviceni cislo 3 zkraceno a vse nepotrebne osekano

--- results/HP_cviceni_3_NejdelsiTitulek.py
+++ results/HP_cviceni_3_NejdelsiTitulek.py
@@ -7,27 +7,19 @@ files_folder = 'C:/Users/hpavlasova/Desktop/PyMum/DictListJSON/json_files/data'
 
 files=os.listdir(files_folder)
 
-Seznam_title=[]
-Seznam_file=[]
 Seznam_komplet=[]
-
-i=-1
 
 for f in files:
 
 	file=os.path.join(files_folder, f)
-
-	Seznam_file.append(f)
-	i+=1
 
 	with open(file, 'r', encoding='utf-8') as data_source:
 		data = json.load(data_source)
 	
 		if 'title' in data:
 			#rovnou pocitam delku titulku a ukladam jen delku
-			Seznam_title.append(len(data['title']))
+			Slovnik={'title':len(data['title']), 'name':f}
 	
-	Slovnik={'title':Seznam_title[i], 'name':Seznam_file[i]}
 	Seznam_komplet.append(Slovnik)
 
 Seznam_komplet.sort(key=lambda k : k['title'])
